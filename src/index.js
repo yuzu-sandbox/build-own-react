07316674a38d0b1abc3cbaa7b424c5705af4f20c
yuzu <s.yuzu441@gmail.com>
2021-05-01 22:28:04 +0900

    Finished step2.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,61 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+/**
+ *
+ * @param {string} type html tag name example for div, p, h1 and etc...
+ * @param {object | null} props
+ * @param  {...any} children
+ * @returns
+ */
+function createElement(type, props, ...children) {
+  return {
+    type,
+    props: {
+      ...props,
+      children: children.map((child) => (typeof child === "object" ? child : createTextElement(child))),
+    },
+  }
+}
 
-const element = <h1 title="foo">Hello</h1>
+function createTextElement(text) {
+  return {
+    type: "TEXT_ELEMENT",
+    props: {
+      nodeValue: text,
+      children: [],
+    },
+  }
+}
+
+/**
+ *
+ * @param {any} element
+ * @param {HTMLElement} container
+ */
+function render(element, container) {
+  const dom = element.type === "TEXT_ELEMENT" ? document.createTextNode("") : document.createElement(element.type)
+
+  const isProperty = (key) => key !== "children"
+  Object.keys(element.props)
+    .filter(isProperty)
+    .forEach((name) => {
+      dom[name] = element.props[name]
+    })
+
+  element.props.children.forEach((child) => render(child, dom))
+
+  container.appendChild(dom)
+}
+
+const Didact = {
+  createElement,
+  render,
+}
+
+/** @jsx Didact.createElement */
+const element = (
+  <div id="foo">
+    <a>bar</a>
+    <b />
+  </div>
+)
 const container = document.getElementById("root")
-ReactDOM.render(element, container)
+Didact.render(element, container)
