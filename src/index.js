e10c27dfe52cf0a8549a647d53081fb4977f0bb1
yuzu <s.yuzu441@gmail.com>
2021-05-03 13:39:40 +0900

    Finished step8.

--- src/index.js
+++ src/index.js
@@ -64,16 +64,30 @@ function commitWork(fiber) {
   if (!fiber) {
     return
   }
-  const domParent = fiber.parent.dom
+
+  let domParentFiber = fiber.parent
+  while (!domParentFiber.dom) {
+    domParentFiber = domParentFiber.parent
+  }
+  const domParent = domParentFiber.dom
+
   if (fiber.effectTag === "PLACEMENT" && fiber.dom != null) {
     domParent.appendChild(fiber.dom)
   } else if (fiber.effectTag === "UPDATE" && fiber.dom != null) {
     updateDom(fiber.dom, fiber.alternate.props, fiber.props)
   } else if (fiber.effectTag === "DELETION") {
-    domParent.removeChild(fiber.dom)
+    commitDeletion(fiber, domParent)
   }
   commitWork(fiber.child)
   commitWork(fiber.sibling)
+}
+
+function commitDeletion(fiber, domParent) {
+  if (fiber.dom) {
+    domParent.removeChild(fiber.dom)
+  } else {
+    commitDeletion(fiber.child, domParent)
+  }
 }
 
 let wipRoot = null
@@ -132,14 +146,12 @@ function workLoop(deadline) {
 requestIdleCallback(workLoop)
 
 function performUnitOfWork(fiber) {
-  // add dom node
-  if (!fiber.dom) {
-    fiber.dom = createDom(fiber)
+  const isFunctionComponent = fiber.type instanceof Function
+  if (isFunctionComponent) {
+    updateFunctionComponent(fiber)
+  } else {
+    updateHostComponent(fiber)
   }
-
-  // create new fibers
-  const elements = fiber.props.children
-  reconcileChildren(fiber, elements)
 
   // return next unit of wordBreak
   if (fiber.child) {
@@ -152,6 +164,23 @@ function performUnitOfWork(fiber) {
     }
     nextFiber = nextFiber.parent
   }
+}
+
+let wipFiber = null
+let hookIndex = null
+function updateFunctionComponent(fiber) {
+  wipFiber = fiber
+  hookIndex = 0
+  wipFiber.hooks = []
+  const children = [fiber.type(fiber.props)]
+  reconcileChildren(fiber, children)
+}
+
+function updateHostComponent(fiber) {
+  if (!fiber.dom) {
+    fiber.dom = createDom(fiber)
+  }
+  reconcileChildren(fiber, fiber.props.children)
 }
 
 function reconcileChildren(wipFiber, elements) {
@@ -209,9 +238,38 @@ function reconcileChildren(wipFiber, elements) {
   }
 }
 
+function useState(initial) {
+  const oldHook = wipFiber.alternate && wipFiber.alternate.hooks && wipFiber.alternate.hooks[hookIndex]
+  const hook = {
+    state: oldHook ? oldHook.state : initial,
+    queue: [],
+  }
+
+  const actions = oldHook ? oldHook.queue : []
+  actions.forEach((action) => {
+    hook.state = action(hook.state)
+  })
+
+  const setState = (action) => {
+    hook.queue.push(action)
+    wipRoot = {
+      dom: currentRoot.dom,
+      props: currentRoot.props,
+      alternate: currentRoot,
+    }
+    nextUnitOfWork = wipRoot
+    deletions = []
+  }
+
+  wipFiber.hooks.push(hook)
+  hookIndex++
+  return [hook.state, setState]
+}
+
 const Didact = {
   createElement,
   render,
+  useState,
 }
 
 const container = document.getElementById("root")
@@ -224,18 +282,43 @@ const container = document.getElementById("root")
 // )
 // Didact.render(element, container)
 
-const updateValue = (e) => {
-  rerender(e.target.value)
-}
+// Step 6 test
+// const updateValue = (e) => {
+//   rerender(e.target.value)
+// }
 
-const rerender = (value) => {
-  const element = (
-    <div>
-      <input onInput={updateValue} value={value} />
-      <h2>Hello {value}</h2>
-    </div>
+// const rerender = (value) => {
+//   const element = (
+//     <div>
+//       <input onInput={updateValue} value={value} />
+//       <h2>Hello {value}</h2>
+//     </div>
+//   )
+//   Didact.render(element, container)
+// }
+
+// rerender("World")
+
+// Step 7 test
+// function App(props) {
+//   return <h1>Hi {props.name}</h1>
+// }
+
+// const element = <App name="foo" />
+// Didact.render(element, container)
+
+//Step 8 text
+function Counter() {
+  const [state, setState] = Didact.useState(1)
+  return (
+    <h1
+      onClick={() => {
+        setState((c) => c + 1)
+      }}
+    >
+      Count: {state}
+    </h1>
   )
-  Didact.render(element, container)
 }
-
-rerender("World")
+const element = <Counter />
+Didact.render(element, container)
